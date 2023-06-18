784ea6ea822a87022e6d63d36067aaed5fe04548
kevin <Elmer@DESKTOP-VN1F8HC>
2023-06-18 20:00:03 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio Button/Ejercicio Button/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio_Button
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Al preciomar el boton va a insertar el texto 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Felicidades";
+        }
+    }
+}
